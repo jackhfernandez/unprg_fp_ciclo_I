187a98f2ef06b3d5a41ce8c7d21224a334df1c3d
Jack <132464970+jackhfernandez@users.noreply.github.com>
2024-12-05 01:36:26 -0500

    Agregando 5,6 y 14

---
+++ problemas_propuestos_parte_1/5_multiplos_10_hasta_n.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+// Mostrar, contar y sumar los multiplos de 10 hasta n
+
+int n;int c=0; int s=0; int i;
+using namespace std;
+int multiplos(int a) {
+  for (i=10; i<=n; i++) {
+    if (i%10==0) {
+      c++;
+      s+=i;
+      cout<<i<<endl;
+    }
+  }
+  return i;
+}
+
+int main() {
+  int m;
+  cout<<"Programa para mostrar, contar y sumar multiplos de 10 hasta n"<<endl;
+  cout<<"Ingrese n: ";
+  cin >>n;
+  multiplos(n);
+  cout<<"\nLa suma es "<<s<<endl<<endl;
+  cout<<"La cantidad de multiplos entre 10 y "<<n<<" es "<<c<<endl;
+
+  return 0;
+}
+
