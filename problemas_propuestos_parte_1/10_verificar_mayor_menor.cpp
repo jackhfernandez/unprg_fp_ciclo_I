a6a16050303919c97800c286bae5d82386a6dcd9
Jack <132464970+jackhfernandez@users.noreply.github.com>
2024-12-07 21:38:19 -0500

    Agregando 9, 10

--- problemas_propuestos_parte_1/10_verificar_mayor_menor.cpp
+++ problemas_propuestos_parte_1/10_verificar_mayor_menor.cpp
@@ -1,36 +1,56 @@
+/* Introducir tantos números como queramos. Decir si son mayores o menores que un
+ número que previamente introduciremos por teclado.*/
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
+void ingresar_base(int &numero_base);
+void almacenar_numeros(vector<int>& numeros);
+void mostrar_resultados(const vector<int>& numeros, int &numero_base);
+
 int main() {
-	int numeroBase;
+
+	vector<int> numeros;
+	int numero_base;
+	cout<<"Programa para verificar si una serie de numeros es mayor que n"<<endl;
+
+	ingresar_base(numero_base);
+	almacenar_numeros(numeros);
+	mostrar_resultados(numeros, numero_base);
+
+	return 0;
+}
+
+void ingresar_base(int &numero_base) {
 	cout<<"Ingrese el numero base: ";
-	cin >>numeroBase;
-	
-	vector<int> numeros; // Vector para almacenar los nros ingresados
-	
-	int numero;
-	char continuar;
-	
+	cin >>numero_base;
+}
+
+void almacenar_numeros(vector<int>& numeros) {
+	int numero; char continuar;
+
 	do {
 		cout<<"Ingrese un numero: ";
 		cin >>numero;
 		numeros.push_back(numero);
-		
-		cout<<"Deseas ingresar otro numero? (s/n): ";
-		cin >> continuar;
-				
-	} while (continuar == 's' || continuar == 'S');
-	
-	cout<<"\nResultados de la comparacion: "<<endl;
-	for (int num : numeros) {
-		if (num > numeroBase) {
-  	  cout<< num << " es mayor que "<< numeroBase<<endl;
-		} else if (num < numeroBase) {
-				cout<< num << " es menor que "<<numeroBase<<endl;
+
+		cout<<"Quieres agregar otro numero? (s/n): ";
+		cin >>continuar;
+	} while (continuar=='s' || continuar=='S');
+}
+
+void mostrar_resultados(const vector<int>& numeros, int &numero_base)  {
+	cout<<"\nResultados"<<endl;
+	for (int numero : numeros) {
+		if (numero > numero_base) {
+  	  cout<< numero << " es mayor que "<< numero_base<<endl;
+		} else if (numero < numero_base) {
+				cout<< numero << " es menor que "<<numero_base<<endl;
 		} else {
-				cout<< num << " es igual a " << numeroBase<<endl;
+				cout<< numero << " es igual a " << numero_base<<endl;
 		}
 	}
-	return 0; 
+	
+	
 }
