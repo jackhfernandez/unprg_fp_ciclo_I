e47135d25fb2826ec1f6cc49e9c3b21fba9d3f3b
Jack <132464970+jackhfernandez@users.noreply.github.com>
2025-01-22 13:16:34 -0500

    Adding arreglo_elem_aleat

---
+++ problemas_de_clase/arreglo_3d_elem_aleat.cpp
@@ -0,0 +1,37 @@
+/* Fernandez
+   Imprimir un arreglo 3D y llenarlos con numeros aleatorios entre 1 y 999
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+  int a = 3, b = 3, c = 3, max = 999, min = 1, arr[a][b][c];
+
+  srand(time(NULL)); // Semilla numeros aleatorios
+
+  cout << "\nImprimir un arreglo 3D con numeros aleatorios\n" << endl;
+
+  // Llenar al arreglo con numeros aleatorios
+  for ( int i = 0; i < a; ++i) {
+    for ( int j = 0; j < b; ++j) {
+      for ( int k = 0; k < c; ++k) {
+        arr[i][j][k] = (rand() % max) + min ;  
+      }
+    }
+  }
+
+  // Imprimir el arreglo
+  for ( int i = 0; i < a; ++i ) {
+    for ( int j = 0; j < b; ++j ) {
+      for ( int k = 0; k < c; ++k ) {
+        cout << setw(3) << arr[i][j][k] << " ";
+      }
+      cout << endl;
+    }
+    cout << endl;
+  }
+
+  return 0;
+}
